1c257c57a217c7509f820f17fa27650b05da1e1f
Corri Golden <corri.golden@gmail.com>
2020-02-06 13:26:58 -0600

    first commit

---
+++ libraryapp/models/book.py
@@ -0,0 +1,23 @@
+from django.db import models #This import is needed for Python classes that are modeling a database table
+from .library import Library
+from .librarian import Librarian
+
+
+class Book (models.Model):
+
+    title = models.CharField(max_length=50)
+    ISBN = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    year_published = models.CharField(max_length=50)
+    location = models.ForeignKey(Library, on_delete=models.CASCADE)
+    librarian = models.ForeignKey(Librarian, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ("Book")
+        verbose_name_plural = ("Books")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Book_detail", kwargs={"pk": self.pk})
