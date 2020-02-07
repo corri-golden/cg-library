63846a01d36403341dc52819998f993b2acadbbb
Corri Golden <corri.golden@gmail.com>
2020-02-07 08:55:56 -0600

    finished chapter 3

--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -1,1 +1,2 @@
 from .books.list import book_list
+from .home import home
---
+++ libraryapp/views/libraries/list.py
@@ -0,0 +1,42 @@
+import sqlite3
+from django.shortcuts import render
+from libraryapp.models import Librarian
+from ..connection import Connection
+
+
+def list_libraries(request):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        select
+            l.id,
+            l.location_id,
+            l.user_id,
+            u.first_name,
+            u.last_name,
+            u.email
+        from libraryapp_librarian l
+        join auth_user u on l.user_id = u.id
+        """)
+
+        all_libraries = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            lib = Library()
+            lib = Library()
+            lib.id = row["id"]
+            lib.title = row["title"]
+            lib.address = row["address"]
+
+            all_libraries.append(lib)
+
+    template_name = 'libraries/list.html'
+
+    context = {
+        'all_libraries': all_libraries
+    }
+
+    return render(request, template_name, context)
